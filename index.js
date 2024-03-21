2418537a8092d08af9b338d9000f545fee0bba13
Jatin <rathorejatin168@gmail.com>
2024-03-21 11:44:19 +0530

    Client url added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(session({
 }));
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://quizify-web.netlify.app"],
+  origin: ["http://localhost:5173", "https://quizify-web.netlify.app", "https://quizify-flax.vercel.app/"],
   credentials: true
 }));
 
