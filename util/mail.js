ab0861e3f2a444cfa252dbd1d05374dfb9f5d340
Jatin <rathorejatin168@gmail.com>
2024-03-21 10:17:39 +0530

    fix: Mail.js configured for live mail sending

--- util/mail.js
+++ util/mail.js
@@ -12,11 +12,12 @@ exports.generateOTP = () => {
 }
 
 exports.mailTransport = () => nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io",
-  port: 2525,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.MAILTRAP_USERNAME,
-    pass: process.env.MAILTRAP_PASSWORD,
+    user: "rathorejatin1234@gmail.com",
+    pass: "nrccvybmbxxuvvgf",
   }
 });
 
