50f653666a99809e72781c7310d8749b4e352c57
Jatin <rathorejatin168@gmail.com>
2024-03-23 18:21:58 +0530

    vite-file updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://quizify-pt2a.onrender.com",
     },
   },
   plugins: [react()],
