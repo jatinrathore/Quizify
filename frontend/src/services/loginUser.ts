f57eb44b7e9c774e0eac584abf8f376b569ec813
Jatin <rathorejatin168@gmail.com>
2024-03-23 16:44:44 +0530

    Debugging Commit

--- frontend/src/services/loginUser.ts
+++ frontend/src/services/loginUser.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { LoginFormData } from "../models/Login";
 
 const loginUser = async (userData: LoginFormData) => {
-  const endpoint = `/api/auth`;
+  const endpoint = `${import.meta.env.VITE_QUIZIFY_SERVER_URL}/api/auth`;
 
   try {
     const { data: res } = await axios.post(endpoint, userData, {
