cd81a18f532bd643bb09385c883ba08b2d07e6d4
Jatin Rathore <rathorejatin168@gmail.com>
2026-03-21 09:27:52 +0530

    chore: added health check for server

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ connectDb();
 
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => {
+const server = app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
 
